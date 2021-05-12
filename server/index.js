5fa02b66204c4d80fb3287a773828bca23a58774
juancedeno96 <cedenojj7@gmail.com>
2021-05-12 11:56:36 -0600

    created routes

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const massive = require('massive')
+const massive = require("massive");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -21,13 +21,66 @@ massive({
 });
 
 //Routes
+//Get Todo
+app.get("/todos", async (req, res) => {
+  try {
+    const db = await req.app.get("db");
+    const allTodos = db.read_todos();
+    return res.status(200).send(allTodos);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
-app.post('/todos', async (req,res)=>{
-try {
+//
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const db = await req.app.get("db");
+    const todo = db.read_todo([id]);
+    res.status(200).send(todo);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//Post Todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const db = await req.app.get("db");
+    const newTodo = db.add_todo(description);
+    return res.status(200).send(newTodo);
+  } catch (err) {
+    console.log(err.message);
+  }
+});
+
+//Edit Todo
+
+app.put('/todos/:id', async (req, res)=>{
+  try {
+    const {id} = req.params
+    const {description} = req.body
+    const db = await req.app.get('db')
+    const updateTodo = db.update_todo([description, id])
+    res.status(202).send(updateTodo)
     
-} catch (err) {
+  } catch (err) {
     console.log(err.message)
-}
+  }
 })
 
+//Delete Todo
 
+app.delete('/todos/:id', async(req,res)=>{
+  try {
+    const {id} = req.params
+    const db = await req.app.get('db')
+    const deleteTodo = db.delete_todo(id)
+    return res.status(200).send(deleteTodo)
+
+  } catch (err) {
+    console.log(err.message)
+  }
+})
